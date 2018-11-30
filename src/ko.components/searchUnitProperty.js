33803920a8dc3fc081522a24f854c4a82f7b85a4
Nurono <while0pass@yandex.ru>
2018-11-30 11:40:54 +0300

    Отображение выбранного свойства вместе с атрибутами запроса в одном блоке

--- src/ko.components/searchUnitProperty.js
+++ src/ko.components/searchUnitProperty.js
@@ -2,55 +2,65 @@ const template = `
 
   <div class="ui top attached segment">
     <div class="ui top attached large label">
-      <header class="bmpp-propertyHeader" data-bind="text: property.name,
-        click: property.onHeaderClick.bind(property),
-        css: { clickable: property.isHeaderClickable }">
+      <header class="bmpp-propertyHeader" data-bind="text: name,
+        click: onHeaderClick.bind($component),
+        css: { clickable: $component.isHeaderClickable }">
       </header>
-      <!-- ko if: property.help -->
+      <!-- ko if: help -->
         <i class="disabled question circle outline icon bmpp-nearLabelIcon"
-          data-bind="popup: property.help"></i>
+          data-bind="popup: help"></i>
       <!-- /ko -->
     </div>
     <div class="ui hidden divider"></div>
 
-    <!-- ko if: property.type === 'text' -->
-      <text-property params="property: property"></text-property>
+    <!-- ko if: type === 'text' -->
+      <text-property params="property: $component"></text-property>
     <!-- /ko -->
 
-    <!-- ko if: property.type === 'interval' -->
-      <interval-property params="property: property"></interval-property>
+    <!-- ko if: type === 'interval' -->
+      <interval-property params="property: $component"></interval-property>
     <!-- /ko -->
 
-    <!-- ko if: property.type === 'list' -->
-      <list-property params="property: property"></list-property>
+    <!-- ko if: type === 'list' -->
+      <list-property params="property: $component"></list-property>
     <!-- /ko -->
   </div>
 
-  <!-- ko if: property.banner -->
-  <div class="ui attached info mini message">
-    <em data-bind="text: property.name"></em>:
-    <span data-bind="text: property.banner"></span>
-  </div>
-  <!-- /ko -->
   <div class="ui bottom attached info mini message" style="margin-bottom: 1em;">
-    <!-- ko if: property.jsonProperties().length === 0 -->
-      Свойство не будет включено в запрос
-    <!-- /ko -->
-    <code>
-    <!-- ko foreach: property.jsonProperties -->
-      <span data-bind="text: JSON.stringify(prop, null, 1)"></span>:
-      <span data-bind="text: JSON.stringify(value, null, 1)"></span><!--
-        ko if: $index() !== $component.property.jsonProperties().length - 1 -->,
+    <div class="ui grid" style="margin: 0!important; padding: 0 !important">
+      <div class="ui eight wide column"
+        style="margin: 0!important; padding: 0 !important">
+
+        <!-- ko if: jsonProperties().length === 0 -->
+          Свойство не будет включено в запрос
         <!-- /ko -->
-    <!-- /ko -->
-    </code>
+        <!-- ko if: banner -->
+          <em data-bind="text: name"></em>:
+          <span data-bind="text: banner"></span>
+        <!-- /ko -->
+
+      </div>
+      <div class="ui eight wide column"
+        style="margin: 0!important; padding: 0 0 0 1em !important;">
+
+        <code style="font-size: smaller;">
+        <!-- ko foreach: jsonProperties -->
+          <span data-bind="text: JSON.stringify(prop, null, 1)"></span>:
+          <span data-bind="text: JSON.stringify(value, null, 1)"></span><!--
+            ko if: $index() !== $component.jsonProperties().length - 1 -->,
+            <!-- /ko -->
+        <!-- /ko -->
+        </code>
+
+      </div>
+    </div>
   </div>
 
 `;
 
 // eslint-disable-next-line no-unused-vars
 var viewModelFactory = (params, componentInfo) => {
-  return { property: params.property };
+  return params.property;
 };
 
 export default {
