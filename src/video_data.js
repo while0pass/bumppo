1748694945ec72907ad9ddc9affa14673c9ef240
Nurono <while0pass@yandex.ru>
2018-12-10 14:36:17 +0300

    Добавлены идентификаторы всех загруженных видео

---
+++ src/video_data.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+export default {
+  '04': {
+    'Neyf': '4j2LpBHEYOY',
+    'N-ey': 'GLuOSjTap-M',
+    'N-vi': 'BlacvxT1Xdk',
+    'C-vi': '4WtA-PbZf-s',
+    'R-vi': '7IGcDVP4YBY',
+    'R-ey': 'VE6Lx58yI3I',
+    'W-vi': 'Jxdu1X_LtaE',
+  },
+  '22': {
+    'Neyf': 'lkKsggceTb4',
+    'N-ey': 'pnLXBP9Jzqk',
+    'N-vi': 'CFFkrk9RwVg',
+    'C-vi': '5nWcsLwsVoY',
+    'R-vi': 'jb8CK2i_EAo',
+    'R-ey': '9xwoE-o9HiY',
+    'W-vi': 'z1C8pfXya-s',
+  },
+  '23': {
+    'Neyf': 'rMRwxgB7tu4',
+    'N-ey': '3-UyrpT3FXM',
+    'N-vi': 'u8N8XXlZbtA',
+    'C-vi': 'TcjujDC0pg0',
+    'R-vi': 'eayOxTz01Vg',
+    'R-ey': 'qszBAwr75R4',
+    'W-vi': 'lW_lNF5gXdQ',
+  },
+};
