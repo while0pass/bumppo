cedf8fa1d0636bd0f880f96ef1aeff1964ec6093
Nurono <while0pass@yandex.ru>
2019-09-24 21:07:10 +0300

    Реализован первый вариант временной шкалы

--- src/ko.components/resultsPane.js
+++ src/ko.components/resultsPane.js
@@ -1,5 +1,8 @@
+/* eslint-disable
+ */
+
 //import ko from 'knockout';
-//import log from '../scripts/log.js';
+import log from '../scripts/log.js';
 //import SVG from 'svg.js';
 //import jQuery from 'jquery';
 import cinema from '../scripts/cinema.js';
@@ -148,11 +151,108 @@ const svgDrawElementId = 'bmpp-timeline',
 
 `;
 
+const MS_IN_S = 1000,
+      S_IN_MIN = 60,
+      MIN_IN_H = 60,
+      H_IN_D = 24;
+
+const MS = 1,
+      CS = 10 * MS,
+      DS = 100 * MS,
+      S = MS_IN_S * MS,
+      DAS = 10 * S,
+      MIN = S_IN_MIN * S,
+      DAMIN = 10 * MIN,
+      H = MIN_IN_H * MIN,
+      DAH = 10 * H,
+      D = H_IN_D * H;
+
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength, padString) {
+    if (this.length >= targetLength) {
+      return String(this);
+    } else {
+      return padString.repeat(targetLength - padString.length) + String(this);
+    }
+  };
+}
+
+function getTimeTag(timePoint, unitScale) {
+  let sign = timePoint >= 0 ? 1 : -1,
+      time = timePoint >= 0 ? timePoint : -timePoint,
+      ms = time % MS_IN_S,
+      s = Math.floor(time / S % S_IN_MIN),
+      min = Math.floor(time / MIN % MIN_IN_H),
+      h = Math.floor(time / H % H_IN_D);
+
+  h = h > 0 ? String(h).padStart(2, '0') : '';
+  min = String(min).padStart(2, '0');
+  s = String(s).padStart(2, '0');
+  ms = String(ms).padStart(3, '0');
+
+  let timeTag = sign < 0 ? '-' : '';
+  if (h) timeTag += h + ':';
+  timeTag += min + ':';
+  timeTag += s;
+  if (unitScale < S) timeTag += '.' + ms;
+  return timeTag;
+}
+
+function makeTimeline(svgDraw, layersStruct) {
+  const { start, end } = layersStruct.time,
+        duration = end - start,
+        width = document.getElementById('bmpp-layersCanvas').clientWidth,
+        referenceWidth = 300,
+        durationPerReferenceWidth = duration * referenceWidth / width;
+
+  log('whole time:', start, end, duration);
+
+  const units = [MS, CS, DS, S, DAS, MIN, DAMIN, H, DAH, D];
+
+  const unitScale = (function () {
+    for (let i = 0; i < units.length - 1; i++) {
+      if (durationPerReferenceWidth < units[i + 1]) { return units[i]; }
+    }
+  })();
+
+  const dUnitScale = unitScale / 10;
+
+  log('scale:', unitScale, dUnitScale);
+
+  const dUnitStartShift = -start % unitScale,
+        ticksN = (Math.floor(duration / unitScale) + 2) * 10 + 1;
+
+  log('shift in dUnits:', dUnitStartShift);
+  log('N ticks:', ticksN);
+
+  const tickStroke = { width: 1, color: '#aaa' },
+        timePointOpts = {
+          'fill': '#aaa',
+          'font-family': 'PT Sans',
+          'font-size': '0.7em',
+          'text-anchor': 'middle',
+        };
+
+  for (let i = 0; i < ticksN; i++) {
+    let isFullUnit = i % 10 === 0,
+        isHalfUnit = i % 5 === 0 && !isFullUnit,
+        tickHeight = isFullUnit ? 10 : isHalfUnit ? 15 : 7,
+        timeShift = dUnitStartShift + i * dUnitScale,
+        x = `${ timeShift / duration * 100 }%`;
+    svgDraw.line(x, 0, x, tickHeight).stroke(tickStroke);
+    if (isFullUnit) {
+      let timePoint = start + timeShift,
+          timeTag = getTimeTag(timePoint, unitScale);
+      svgDraw.text(timeTag).attr(timePointOpts).attr({ x: x, y: 20 });
+    }
+  }
+}
+
 const template = videoTemplate +
   queryInfoTemplate + resultsTemplate + layersTemplate;
 
 function viewModelFactory(params) {
-  let //svgDraw = SVG(svgDrawElementId).size('100%', '100%'),
+  let svgDraw = SVG(svgDrawElementId).size('100%', '100%'),
       layersStruct = new LayersStruct(params.layersData()),
       elNC = document.getElementById('bmpp-lNContainer'),
       elLL = document.getElementById('bmpp-layersLayers'),
@@ -161,7 +261,7 @@ function viewModelFactory(params) {
         event.preventDefault();
         elNC.scrollTop = elLL.scrollTop -= event.wheelDelta;
       };
-  //makeTimeline(svgDraw);
+  makeTimeline(svgDraw, layersStruct);
   elLL.addEventListener('scroll', propagateScroll);
   elNC.addEventListener('wheel', propagateScrollReverse);
   return {
