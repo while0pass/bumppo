2a2d78f413b8dc1505fa659caedf4542151a6126
Nurono <while0pass@yandex.ru>
2020-02-16 19:12:46 +0300

    Отказ от grid при верстке результатов из-за бага в хроме
    https://bugs.chromium.org/p/chromium/issues/detail?id=1052671

--- src/ko.components/resultsList.js
+++ src/ko.components/resultsList.js
@@ -1,31 +1,33 @@
+import ko from 'knockout';
+
 const template = `
 
   <div class="ui basic segment">
+  <div class="bmpp-searchResults" data-bind="foreach: resultsData">
 
-    <div class="bmpp-searchResults" data-bind="foreach: resultsData">
+    <div class="ui label bmpp-recordLabel" data-bind="
+      text: 'Запись ' + record_id,
+      visible: !previousItem || previousItem
+                             && previousItem.record_id !== record_id,
+      css: { currentItem: $data === $component.cinema.activeDataItem(),
+             hoveredItem: $data === $component.hoveredItem() }">
+    </div>
 
-      <div class="divider" data-bind="visible: previousItem"></div>
+    <div class="bmpp-result" data-bind="
+      event: { mouseover: $component.hoveredItem },
+      css: { currentItem: $data === $component.cinema.activeDataItem() }">
 
-      <div class="bmpp-time"
-        data-bind="text: match.beginTime + '–' + match.endTime"></div>
-
+      <div class="bmpp-time" data-bind="text: match.beginTime + '–' +
+                                              match.endTime"></div>
       <div class="bmpp-duration" data-bind="text: match.duration"></div>
-
       <div class="bmpp-unitValue" data-bind="text: match.value"></div>
-
-      <div class="bmpp-transcription"
-        data-bind="html: match.transcription, click: $component.showFilm,
-          css: { currentItem: $data === $component.cinema.activeDataItem() }">
-      </div>
-
-      <div class="ui label bmpp-recordLabel"
-        data-bind="text: 'Запись ' + record_id,
-          visible: !previousItem
-                || previousItem && previousItem.record_id !== record_id">
+      <div class="bmpp-transcription" data-bind="click: $component.showFilm">
+        <div data-bind="html: match.transcription"></div>
       </div>
 
     </div>
 
+  </div>
   </div>
 
 `;
@@ -34,7 +36,8 @@ var viewModelFactory = function (params) {
   var vM = params.viewModel,
       activeResult = params.activeResult,
       cinema = vM.cinema,
-      resultsData = params.resultsData;
+      resultsData = params.resultsData,
+      hoveredItem = ko.observable(null);
 
   function showFilm(data) {
     if (data !== cinema.activeDataItem()) {
@@ -45,7 +48,7 @@ var viewModelFactory = function (params) {
     cinema.showFilm(data.record_id, data.filmType, data);
   }
 
-  return { cinema, resultsData, showFilm };
+  return { vM, cinema, resultsData, showFilm, hoveredItem };
 };
 
 export default {
